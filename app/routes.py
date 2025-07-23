0c7b17997590e739ca257836d62d874859e499d4
Parth <frostgameing0@gmail.com>
2025-07-23 18:39:47 +0530

    added routes

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,23 @@ from flask import render_template, request
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/docs")
+def docs():
+    return render_template("docs.html")
+
+@app.route("/team")
+def team():
+    return render_template("team.html")
+
+@app.route("/explore")
+def explore():
+    return render_template("explore.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
